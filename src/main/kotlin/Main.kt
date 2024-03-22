b245fde45bebe5e60379c1c514f1c542e8b1b6b8
Michael Depies <depiesms22@gmail.com>
2024-03-22 10:50:13 -0500

    pulled impl of MapElitesOperator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,23 +31,3 @@ fun main() {
     mapElites.initialize(100)
     mapElites.evolve(1000)
 }
-class EliteMapOperatorImpl(val featureDescriptor: (Solution) -> List<Int>, val map: MutableMap<List<Int>, Solution>) : EliteMapOperator {
-    override fun placeInMap(solution: Solution) {
-        val cellIndices = featureDescriptor(solution)
-        val cell = map[cellIndices]
-        if (cell == null || solution.performance > cell.performance) {
-            map[cellIndices] = solution
-        }
-    }
-    override fun getCell(cellIndices: List<Int>): Solution? {
-        return map[cellIndices]
-    }
-
-
-    override fun selectParents(): Pair<Solution, Solution> {
-        //select two solutions from the map
-        val cell = map.values.random()
-        val cell2 = map.values.random()
-        return Pair(cell, cell2)
-    }
-}
