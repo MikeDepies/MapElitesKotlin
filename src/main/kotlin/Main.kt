8f77ee32526f11b3ef4491c5fda2c37f1c02138d
Michael Depies <depiesms22@gmail.com>
2024-03-22 16:55:31 -0500

    more refactoring around the separation of programModel and solutionModel.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,8 @@ package ai.sunnystratgies
 import ai.sunnystratgies.mapelites.*
 import java.util.UUID
 data class ModelProgram(val id: String, val programText : String)
-interface ProgramBehaviorEvaluator {
-    fun evaluate(program: ModelProgram): Solution
+interface ProgramBehaviorEvaluator<T> {
+    fun evaluate(program: T): Solution
 }
 fun main() {
     // Define the feature descriptor, mutation, crossover, and evaluate functions
@@ -12,42 +12,58 @@ fun main() {
     val featureDescriptorTransformer = FeatureDescriptorTransformer(metaList)
     val featureDescriptor = featureDescriptorTransformer::featureDescriptor
     val programMap = ProgramMap<ModelProgram>()
-    // val programBehaviorEvaluator : (ModelProgram) -> List<Double> = { modelProgram ->
-    //     val programText = modelProgram.programText
-    //     TODO()
-    // }
-    val eliteMutator = object : EliteMutator<ModelProgram> {
-        override fun mutate(solution: ModelProgram): ModelProgram {
-            TODO("Not yet implemented")
-        }
-
-        override fun crossover(parent1: ModelProgram, parent2: ModelProgram): ModelProgram {
-            TODO("Not yet implemented")
-        }
-
-        override fun generateRandomSolution(): ModelProgram {
-            val id = UUID.randomUUID().toString()
-            val programText = "def main():\n  print('hello world')"
-            val modelProgram = ModelProgram(id, programText)
-            programMap.addProgram(id, modelProgram)
-            return modelProgram
-        }
-    }
-    val programEvaluator = object : ProgramBehaviorEvaluator {
-        override fun evaluate(program: ModelProgram): Solution {
-            // val programText = program.programText
-            TODO()
-        }
-    }
-    val eliteEvaluator = object : EliteEvaluator<ModelProgram> {
-        override fun evaluate(program: ModelProgram): Solution {
-            return programEvaluator.evaluate(program)
-        }
-    }
+    val eliteMutator = EliteMutatorImpl(programMap)
+    val programEvaluator = ProgramEvaluatorImpl()
+    val eliteEvaluator = EliteEvaluatorImpl(programEvaluator)
     val eliteMapOperator = EliteMapOperatorImpl(featureDescriptor, mutableMapOf())
     val mapElites = MapElitesAlgorithmImpl<ModelProgram>(programMap,eliteMapOperator, eliteMutator, eliteEvaluator, MapElitesConfiguration((.5)))
     mapElites.initialize(100)
     mapElites.evolve(1000)
+}
+class ProgramEvaluatorImpl : ProgramBehaviorEvaluator<ModelProgram> {
+    override fun evaluate(program: ModelProgram): Solution {
+        val programText = program.programText
+        val programBehavior = programBehaviorEvaluator(programText)
+        return Solution(program.id, programBehavior, 0.0)
+    }
+
+    fun programBehaviorEvaluator(programText: String): List<Double> {
+        TODO(programText)
+    }
+}
+class EliteMutatorImpl(val programMap: ProgramMap<ModelProgram>) : EliteMutator<ModelProgram> {
+    override fun mutate(solution: ModelProgram): ModelProgram {
+        val programText = solution.programText
+        val programTextMutated = programText
+        val id = UUID.randomUUID().toString()
+        val modelProgram = ModelProgram(id, programTextMutated)
+        programMap.addProgram(id, modelProgram)
+        return modelProgram
+    }
+
+    override fun crossover(parent1: ModelProgram, parent2: ModelProgram): ModelProgram {
+        val programText1 = parent1.programText
+        val programText2 = parent2.programText
+        val programTextMutated = programText1 + programText2
+        val id = UUID.randomUUID().toString()
+        val modelProgram = ModelProgram(id, programTextMutated)
+        programMap.addProgram(id, modelProgram)
+        return modelProgram
+    }
+
+    override fun generateRandomSolution(): ModelProgram {
+        val id = UUID.randomUUID().toString()
+        val programText = "def main():\n  print('hello world')"
+        val modelProgram = ModelProgram(id, programText)
+        programMap.addProgram(id, modelProgram)
+        return modelProgram
+    }
+}
+class EliteEvaluatorImpl(val programBehaviorEvaluator: ProgramBehaviorEvaluator<ModelProgram>) : EliteEvaluator<ModelProgram> {
+    override fun evaluate(program: ModelProgram): Solution {
+        val solution = programBehaviorEvaluator.evaluate(program)
+        return solution
+    }
 }
 
 
